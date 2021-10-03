ef71a24f27498cd7a5394cc445aa593b92b869ee
Asraful Islam <iasraful321@gmail.com>
2021-10-03 13:46:38 +0600

    Set up the project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+//Styles
+import "./assets/styles/App.scss";
+
+const App = () => {
+  return (
+    <div className='container'>
+      <h3>Hey everyone! Lets learn about React Three Fiber :)</h3>
+      <p>
+        Dont forget to subscribe
+        <a href='https://youtube.com/c/wrongakram'>@wrongakram</a>
+      </p>
+    </div>
+  );
+};
+
+export default App;
