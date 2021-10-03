5d37d4302ca7e1a98fe6cc4beda9a2f28f7673b0
Asraful Islam <iasraful321@gmail.com>
2021-10-03 13:59:42 +0600

    Set up the project

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import React from "react";
 import "./assets/styles/App.scss";
 
 const App = () => {
-  return (
-    <div className='container'>
-      <h3>Hey everyone! Lets learn about React Three Fiber :)</h3>
-      <p>
-        Dont forget to subscribe
-        <a href='https://youtube.com/c/wrongakram'>@wrongakram</a>
-      </p>
-    </div>
-  );
+  return <div className="container"></div>;
 };
 
 export default App;
